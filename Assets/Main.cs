bdc443139e71ddfafa388d63a210a147a455be0a
Loren Kuich <loren.jk3@gmail.com>
2018-03-08 01:26:44 -0700

    Initial commit

---
+++ Assets/Main.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TensorFlow;
+
+public class Main : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        TensorFlowSharp.Android.NativeBinding.Init();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
