92a39619e3715b8adfdd85140c37f1107a7802ac
Loren Kuich <lkuich@agentsofdiscovery.com>
2018-03-08 17:41:28 -0800

    Worked on implementing tensor

--- Assets/Main.cs
+++ Assets/Main.cs
@@ -2,16 +2,63 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TensorFlow;
+using UnityEngine.UI;
 
-public class Main : MonoBehaviour {
+public class Tensor {
 
-	// Use this for initialization
-	void Start () {
+    private string[] Labels = { "tulips", "roses", "dandelion", "sunflowers", "daisy" };
+
+    public Text output;
+
+    private TextAsset graphModel;
+    private TFGraph graph;
+    private TFSession session;
+
+    // Use this for initialization
+    public Tensor(Text output) {
+        this.output = output;
+
         TensorFlowSharp.Android.NativeBinding.Init();
+        graphModel = Resources.Load("1.4/graph") as TextAsset;
     }
 
-    // Update is called once per frame
-    void Update () {
-		
-	}
+    public void Import()
+    {
+        output.text = "importing graph";
+
+        graph = new TFGraph();
+        graph.Import(graphModel.bytes);
+        session = new TFSession(graph);
+
+        if (session != null)
+            output.text += "\nimported graph";
+    }
+
+    public void ExParse(byte[] bytes)
+    {
+        var runner = session.GetRunner();
+        if (runner != null)
+            output.text += "\ngot runner";
+
+        runner.AddInput(graph["input_placeholder_name"][0], new float[] { 0, 1 });
+        runner.Fetch(graph["output_placeholder_name"][0]);
+        float[,] recurrent_tensor = runner.Run()[0].GetValue() as float[,];
+    }
+    
+    public float[,] Parse(byte[] bytes)
+    {
+        if (bytes != null)
+        {
+            var tensor = new TFTensor(bytes);
+            var runner = session.GetRunner();
+            Debug.Log("runner is not null");
+            
+            runner.AddInput(graph["encoded_image_bytes"][0], tensor);
+            runner.Fetch("probabilities");
+            return runner.Run()[0].GetValue() as float[,];
+        }
+        else
+            output.text = "ERROR: Bytes null";
+        return new float[,] { };
+    }
 }
---
+++ Assets/WebCamPanel.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WebCamPanel : MonoBehaviour
+{
+    public Text output;
+    public RawImage image;
+    public Button CaptureButton;
+    private WebCamTexture _webCamTexture;
+
+    private Tensor t;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(SetupCoroutine());
+        CaptureButton.onClick.AddListener(() => StartCoroutine(TakePhoto()));
+
+        t = new Tensor(output);
+        t.Import();
+    }
+
+    public IEnumerator TakePhoto()
+    {
+        // NOTE - you almost certainly have to do this here:
+
+        yield return new WaitForEndOfFrame();
+
+        // it's a rare case where the Unity doco is pretty clear,
+        // http://docs.unity3d.com/ScriptReference/WaitForEndOfFrame.html
+        // be sure to scroll down to the SECOND long example on that doco page 
+
+        Texture2D photo = new Texture2D(_webCamTexture.width, _webCamTexture.height);
+        photo.SetPixels(_webCamTexture.GetPixels());
+        photo.Apply();
+
+        //Encode to a PNG
+        byte[] bytes = photo.EncodeToPNG();
+        var outp = t.Parse(bytes);
+        output.text = "";
+        foreach (float f in outp)
+        {
+            output.text += f.ToString();
+        }
+
+        //Write out the PNG. Of course you have to substitute your_path for something sensible
+        File.WriteAllBytes("photo.png", bytes);
+    }
+
+    private IEnumerator SetupCoroutine()
+    {
+        while (_webCamTexture == null)
+        {
+            WebCamDevice[] devices = WebCamTexture.devices;
+            if (devices.Length > 0)
+            {
+                _webCamTexture = new WebCamTexture(devices[0].name);
+                _webCamTexture.Play();
+
+                yield return new WaitUntil(() => _webCamTexture.width > 10);
+
+                image.texture = _webCamTexture;
+                image.material.mainTexture = _webCamTexture;
+                image.color = Color.white;
+                bool rotated = TransformImage();
+                SetImageSize(rotated);
+            }
+            yield return 0;
+        }
+    }
+
+    private void SetImageSize(bool rotated)
+    {
+        var canvas = gameObject.GetComponentInParent<Canvas>();
+        var canvasRect = canvas.pixelRect;
+        var rotTexSize = rotated ? new Vector2(_webCamTexture.height, _webCamTexture.width) : new Vector2(_webCamTexture.width, _webCamTexture.height);
+        float ratio = 1f;
+        if (rotTexSize.x < canvasRect.width && rotTexSize.y > canvasRect.height)
+        {
+            ratio = canvasRect.width / rotTexSize.x;
+        }
+        else if (rotTexSize.x > canvasRect.width && rotTexSize.y < canvasRect.height)
+        {
+            ratio = canvasRect.height / rotTexSize.y;
+        }
+        else if (rotTexSize.x < canvasRect.width && rotTexSize.y < canvasRect.height)
+        {
+            var widthRatio = canvasRect.width / rotTexSize.x;
+            var heightRatio = canvasRect.height / rotTexSize.y;
+            ratio = widthRatio < heightRatio ? heightRatio : widthRatio;
+        }
+        else if (rotTexSize.x > canvasRect.width && rotTexSize.y > canvasRect.height)
+        {
+            var widthRatio = canvasRect.width / rotTexSize.x;
+            var heightRatio = canvasRect.height / rotTexSize.y;
+            ratio = widthRatio < heightRatio ? heightRatio : widthRatio;
+        }
+
+        var rect = image.gameObject.GetComponent<RectTransform>();
+        var texSize = new Vector2(_webCamTexture.width, _webCamTexture.height);
+        rect.sizeDelta = texSize * ratio / canvas.scaleFactor;
+    }
+
+    private bool TransformImage()
+    {
+#if UNITY_IOS
+		// iOS cam is mirrored
+        image.gameObject.transform.localScale = new Vector3(-1, 1, 1);
+#endif
+        image.gameObject.transform.Rotate(0.0f, 0, -_webCamTexture.videoRotationAngle);
+        return _webCamTexture.videoRotationAngle != 0;
+    }
+
+    private void OnDestroy()
+    {
+        _webCamTexture.Stop();
+        Destroy(_webCamTexture);
+    }
+}
